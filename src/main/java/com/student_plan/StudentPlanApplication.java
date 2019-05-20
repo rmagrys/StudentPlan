4ae5abf42a569eecb9209d4a4bde1f4a210864bb
CaptainHawkk <r.magrys@wp.pl>
2019-05-20 18:15:17 +0200

    basic project template

---
+++ src/main/java/com/student_plan/StudentPlanApplication.java
@@ -0,0 +1,13 @@
+package com.student_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentPlanApplication.class, args);
+    }
+
+}
